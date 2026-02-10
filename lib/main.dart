2dd8a51f8ea722feb6517a15928d065da9ee92d7
Erica Esteves <ericaesteves95@gmail.com>
2026-02-10 19:38:17 -0300

    Integrando GetX e atualizando o roteamento do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:greengrocer/src/pages/splash/splash_screen.dart';
+import 'package:get/get.dart';
+import 'package:greengrocer/src/routes/app_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Greengrocer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      initialRoute: PagesRoutes.splashRoute,
+      getPages: AppPages.pages,
     );
   }
 }
