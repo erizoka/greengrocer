1ab29d89f86a2ab5ae8ba2f12d01ae5a51039e6b
Erica Esteves <ericaesteves95@gmail.com>
2026-02-18 12:28:41 -0300

    Placeholder para caso não haja nenhum pedido para o usuário

--- lib/src/pages/orders/view/orders_tab.dart
+++ lib/src/pages/orders/view/orders_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/orders/controller/all_orders_controller.dart';
 import 'package:greengrocer/src/pages/orders/view/components/order_tile.dart';
 
@@ -12,13 +13,30 @@ class OrdersTab extends StatelessWidget {
       appBar: AppBar(title: const Text('Pedidos')),
       body: GetBuilder<AllOrdersController>(
         builder: (controller) {
-          return ListView.separated(
-            padding: const EdgeInsets.all(16),
-            physics: const BouncingScrollPhysics(),
-            separatorBuilder: (_, i) => const SizedBox(height: 10),
-            itemCount: controller.allOrders.length,
-            itemBuilder: (_, i) => OrderTile(order: controller.allOrders[i]),
-          );
+          return controller.allOrders.isEmpty
+              ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(
+                    Icons.label_off,
+                    size: 40,
+                    color: CustomColors.customSwatchColor,
+                  ),
+                  const Text(
+                    'Ainda não foi feito nenhum pedido',
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              )
+              : ListView.separated(
+                padding: const EdgeInsets.all(16),
+                physics: const BouncingScrollPhysics(),
+                separatorBuilder: (_, i) => const SizedBox(height: 10),
+                itemCount: controller.allOrders.length,
+                itemBuilder:
+                    (_, i) => OrderTile(order: controller.allOrders[i]),
+              );
         },
       ),
     );
